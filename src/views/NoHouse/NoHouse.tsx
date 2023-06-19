2ffeb1bb05cfb13fe1550d08f19fa966a2fc03c7
Robert Acsinte <robert.acsinte@gmail.com>
2023-06-19 12:12:56 +0200

    No House UI Design

--- src/views/NoHouse/NoHouse.tsx
+++ src/views/NoHouse/NoHouse.tsx
@@ -1,5 +1,6 @@
 import { useAuthContext } from '../../context/AuthContext';
 import Navbar from '../../components/Navbar/Navbar';
+import style from './NoHouse.module.css'
 
 function NoHouse() {
   const context = useAuthContext();
@@ -9,7 +10,16 @@ function NoHouse() {
   return (
     <>
       <Navbar showAllOptions = {false}/>
-      <div>{currentUserDataDb!.name}</div>
+
+      <div className='center-wrapper'>
+        <div className={style.noHouseText}>Currently you are not part of any house!</div>
+        <div className={style.container}>
+          <div className={style.buttonContainer}>
+            <button className='full-button' style={{marginRight: '20px'}}>Join an existing house</button>
+            <button className='empty-button' style={{marginLeft: '20px'}}>Create a new house</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
