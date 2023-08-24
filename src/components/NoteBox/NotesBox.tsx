11ce5ed25b86c03bfda4c88a28b3d9f833df760f
Robert Acsinte <robert.acsinte@gmail.com>
2023-08-24 13:33:00 +0300

    Time for notes shown as "13:09" instead of "13:9" (added a 0 before if lower than 10 for better readability)

--- src/components/NoteBox/NotesBox.tsx
+++ src/components/NoteBox/NotesBox.tsx
@@ -8,11 +8,22 @@ type NotesBoxProps = {
 }
 
 function NotesBox({title, content, date, user}: NotesBoxProps) {
+  var minutes: string | number = new Date(date).getMinutes() 
+  var hours: string | number = new Date(date).getHours() 
+
+  if(minutes < 10) {
+    minutes = "0" + minutes
+  }
+
+  if(hours < 10) {
+    hours = "0" + hours
+  }
+
   return (
     <div className={style.container}>
       <p className={style.title}>{title}</p>
       <b>{user}</b>
-      <p className={style.date}>{new Date(date).toLocaleDateString("nl-NL")} {new Date(date).getHours()}:{new Date(date).getMinutes()}</p>
+      <p className={style.date}>{new Date(date).toLocaleDateString("nl-NL")} {hours}:{minutes}</p>
       <p className={style.content}>{content}</p>
     </div>
   )
