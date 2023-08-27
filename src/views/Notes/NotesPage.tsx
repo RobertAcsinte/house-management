44aac9cfa2952600f70fdc2449fe9cb8f86e41e2
Robert Acsinte <robert.acsinte@gmail.com>
2023-08-27 18:21:13 +0300

    Improved notes layout on desktop and mobile

--- src/views/Notes/NotesPage.tsx
+++ src/views/Notes/NotesPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import style from './NotesPage.module.css'
 import ModalAddNote from '../../components/ModalAddEditNote/ModalAddEditNote'
@@ -10,6 +10,7 @@ function NotesPage() {
   const [showModal, setShowModal] = useState<boolean>(false)
   const modal = useRef<JSX.Element | null>(null)
   const notesContext = useNotesContext()
+  const [isMobile, setIsMobile] = useState<boolean>(false)
 
   const onAddButton = () => {
     setShowModal(true)
@@ -20,6 +21,29 @@ function NotesPage() {
     setShowModal(true)
     modal.current = <NoteDetails setShowModal={setShowModal} note={note}/>
     document.body.style.overflow = 'hidden';
+  }
+
+  const handleResize = () => {
+    if(window.innerWidth <= 800) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+  }, [])
+
+  var gridStyle = 'repeat(auto-fit, minmax(200px, 300px))'
+  var gridGap = '40px'
+
+  if(notesContext.notes){
+    if(notesContext.notes.length > 2 || isMobile) {
+      gridStyle = 'repeat(auto-fit, minmax(300px, 1fr))'
+      gridGap = '20px'
+    } 
+    
   }
 
   const notes = notesContext.notes?.map((note) => {
@@ -40,7 +64,7 @@ function NotesPage() {
           Add a note
         </button>
       </div>
-      <div className={style.notesGrid}>
+      <div className={style.notesGrid} style={{gridTemplateColumns: gridStyle, columnGap: gridGap}}>
           {notes}
       </div>
       <div className='error-wrapper'>
