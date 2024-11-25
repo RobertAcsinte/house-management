a0826b1b5d362bc2a1675cba436bfc4d4e000499
Robert Acsinte <robert.acsinte@gmail.com>
2024-11-25 13:37:29 +0100

    removed unused old code from RegisterPage component | uploaded file is a ref instead of a state

--- src/features/users/Register/RegisterPage.tsx
+++ src/features/users/Register/RegisterPage.tsx
@@ -1,111 +1,46 @@
-import { useEffect, useState } from 'react';
+import React, {useRef, useState} from 'react';
 import { ClipLoader } from 'react-spinners';
-import mapErrorMessages from '../../../mapErrorMessages.tsx';
-import { useAuthContext } from '../../../context/AuthContext.tsx';
-import Logo from '../../../assets/logo.svg';
 import Avatar from '../../../assets/default.png'
-import { updateProfile } from 'firebase/auth';
 import style from './RegisterPage.module.scss'
 import {FormProvider, useForm} from "react-hook-form";
 import Input from "../../../components/Input/Input.tsx";
 import {emailValidation, fieldNotEmpty, passwordValidation} from "../../../utils/validations.tsx";
-import {useNavigate} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../../withTypes.ts";
+import {registerUser} from "../usersSlice.ts";
 
 
 type Inputs = {
   email: string,
-  displayName:string,
+  name:string,
   password: string,
   repeatPassword: string
 }
 
 function RegisterPage() {
-  const navigate = useNavigate()
   const methods = useForm<Inputs>()
   const dispatch = useAppDispatch()
   const {error, status} = useAppSelector(state => state.user)
-
-  const context = useAuthContext()
-  // const [loading, setLoading] = useState<boolean>(false)
-  // const [error, setError] = useState<string | null>(null)
-  const [imgLoading, setImgLoading] = useState(true)
   const [photoURL, setPhotoURL] = useState<string>()
-  const [file, setFile] = useState<Blob | Uint8Array | ArrayBuffer>()
+  const fileRef = useRef<Blob | Uint8Array | ArrayBuffer>()
 
-  const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget as HTMLFormElement);
-    const email = formData.get("email") as string
-    const name = formData.get("name") as string
-    const password = formData.get("password") as string
-    const repeatPassword = formData.get("repeatPassword") as string
-
-    if (!email || !name || !password || !repeatPassword) {
-      setError("Please fill out all the fields.")
-      return
-    }
-    if (repeatPassword !== password) {
-      setError("Passwords don't match.")
-      return
-    }
-
-    setLoading(true)
-
-
-    const userCredential = await context.register(email, password).catch((registerError) => {
-      setLoading(false)
-      setError(mapErrorMessages(registerError.code))
-    })
-
-    if (userCredential) {
-      if(file) {
-        await context.uploadAvatar(file).catch((error) => {
-          setLoading(false)
-          setError(error)
-        })
-      } else {
-        const path = "../../../public/default.png"
-        const response = await fetch(path);
-        const blob = await response.blob();
-        await context.uploadAvatar(blob).catch((error) => {
-          setLoading(false)
-          setError(error)
-        })
-      }
-      
-      await context.saveUserDb(userCredential.user.uid, email, name).catch((registerError) => {
-        setLoading(false)
-        setError(mapErrorMessages(registerError.code))
-      })
-      await context.login(email, password, true).catch((loginError) => {
-        setLoading(false)
-        setError(mapErrorMessages(loginError.code))
-      })
-      setLoading(false)
-    }
-  }
-
-  function handleLoad () {
-    setImgLoading(false)
-  }
+  const onSubmit = methods.handleSubmit(async (data: Inputs) => {
+    dispatch(registerUser({
+      email: data.email, displayName: data.name, password: data.password, repeatPassword: data.repeatPassword, avatar: fileRef.current}))
+  })
 
   const loadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      setFile(event.target.files[0])
+      fileRef.current = event.target.files[0]
       setPhotoURL(URL.createObjectURL(event.target.files[0]));
-      setError(null)
     }
   }
-
-
 
   return (
       <main>
         <h1 className="visually-hidden">WeShare Register Page</h1>
         <section className='wrapper center'>
           <div className='box'>
-            <img className={style.avatar} src={Avatar} onLoad={handleLoad}/>
+            <img className={style.avatar} src={photoURL || Avatar} alt="Picked avatar"/>
             <label className={style['custom-select-file-label']}>
               <input type="file" onChange={loadFile} accept="image/*"/>
               Select photo
@@ -129,30 +64,6 @@ function RegisterPage() {
           </div>
         </section>
       </main>
-      // <>
-      //   <div className='center-wrapper-nonav'>
-      //     <div className='box-container'>
-      //       <div style={{display: imgLoading ? "block" : "none"}}>
-      //         <div className='spinner-button'>
-      //           <ClipLoader color="var(--secondary)" size="50px" />
-      //         </div>
-      //       </div>
-      //       <img className={style.avatar} src= {photoURL} onLoad={handleLoad} style={{display: imgLoading ? "none" : "block"}}/>
-      //       <label className={style.custom}>
-      //       <input type="file" onChange={loadFile} accept="image/*"/>
-      //       Select photo
-      //       </label>
-      //       <form onSubmit={event => onSubmit(event)}>
-      //         <input type="text" placeholder='Email' name='email' />
-      //         <input type="text" placeholder='Name' name='name' />
-      //         <input type="password" placeholder='Password' name='password' />
-      //         <input type="password" placeholder='Repeat Password' name='repeatPassword' />
-      //         {loading ? <div className='spinner-button' style={{marginTop:"65px"}}><ClipLoader color="var(--secondary)" size="50px" /> </div> : <button type="submit" className='full-button'>Register</button>}
-      //       </form>
-      //       <div className='error-text'>{error}</div>
-      //     </div>
-      //   </div>
-      // </>
   )
 }
 
