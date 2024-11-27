8665ecc9984988501106fdbb9bca267ec80bcfa0
Robert Acsinte <robert.acsinte@gmail.com>
2024-11-27 12:49:08 +0100

    test for invalid email and passwords fields Login Page

--- src/features/users/Login/LoginPage.test.tsx
+++ src/features/users/Login/LoginPage.test.tsx
@@ -1,8 +1,16 @@
-import { expect, it } from 'vitest'
+import { expect, it, vi } from 'vitest'
 import LoginPage from "./LoginPage.tsx";
 import {renderWithProviders} from "../../../utils/test-utils.tsx";
+import {fireEvent, screen} from '@testing-library/react'
+
 
 it("Snapshot Login Page", () => {
     const snapshot = renderWithProviders(<LoginPage />)
     expect(snapshot).toMatchSnapshot()
+})
+
+it("Invalid email format and password fields", async () => {
+    renderWithProviders(<LoginPage/>)
+    fireEvent.click(screen.getByText('Login'))
+    expect(await screen.findAllByRole("alert")).toHaveLength(2);
 })
